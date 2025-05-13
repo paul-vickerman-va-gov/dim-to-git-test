5de9cfa57bf01af9ee0d940e62bfe8cb07d82398
OITCOVICKEP <OITCOVICKEP@PAL-LT223156>
2025-05-13 09:07:41 -0700

    # Change Set 89 by S317JXD at 2019-06-10 07:06:31: "Changed the keystore to come in through a config file and JKSKeyManager, added validation of Assertion signature"

--- security/src/main/java/gov/va/vba/css/security/service/handler/WSSRequiredSignatureHandler.java
+++ security/src/main/java/gov/va/vba/css/security/service/handler/WSSRequiredSignatureHandler.java
@@ -1,8 +1,5 @@
 package gov.va.vba.css.security.service.handler;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.PublicKey;
@@ -19,26 +16,19 @@ import javax.xml.soap.SOAPPart;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
 import org.apache.ws.security.WSConstants;
 import org.apache.ws.security.util.WSSecurityUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.saml.key.JKSKeyManager;
 import org.springframework.util.Assert;
 import org.w3c.dom.Element;
 import gov.va.vba.css.security.exceptions.CssWsException;
-import gov.va.vba.css.security.formatters.BasicXmlFormatter;
-import gov.va.vba.css.security.formatters.XmlFormatter;
 import gov.va.vba.framework.logging.Logger;
 
 public class WSSRequiredSignatureHandler extends CSSWSHandler {
 
 	private static Logger logger = Logger.getLogger(WSSRequiredSignatureHandler.class);
-    
-    private XmlFormatter formatter = new BasicXmlFormatter();
-
-    public XmlFormatter getFormatter() {
-        return formatter;
-    }
-
-    public void setFormatter(XmlFormatter formatter) {
-        this.formatter = formatter;
-    }
+	
+	@Autowired
+	private JKSKeyManager keyManager;
     
     @Override
     public boolean handleFault(SOAPMessageContext context) {
@@ -46,28 +36,21 @@ public class WSSRequiredSignatureHandler extends CSSWSHandler {
     }
     
     private KeyStore getKeystore() throws CssWsException{
-    	InputStream inStream = null;
 		KeyStore ks = null;
 		
     	try {
-    		//TODO bring these in from properties file
-			inStream = new FileInputStream("css_props/cssKeystore.jks");//keystore
-			ks = KeyStore.getInstance("JKS");
-			ks.load(inStream, "csskey".toCharArray());//keystore password
-			
+    		if (keyManager == null) {
+    			logger.error("Error getting keystore, key manager null ");
+    			throw new CssWsException("Unable to get key manager ");
+    		} else {
+    			logger.debug("Key manager available credentials: " + keyManager.getAvailableCredentials().toString());
+    			ks = keyManager.getKeyStore();
+    		}
 		} catch (Exception e) {
 			logger.error("Error getting keystore " + e.getMessage());
             e.printStackTrace(System.out);
             throw new CssWsException("Unable to get keystore ", e);
-		} finally {
-			if (inStream != null) {
-				try {
-					inStream.close();
-				} catch (IOException e) {
-					logger.error("Error closing keystore inStream " + e.getMessage());
-				}
-			}
-		}
+		} 
     	
     	return ks;
     }
@@ -91,7 +74,6 @@ public class WSSRequiredSignatureHandler extends CSSWSHandler {
 	
 	@Override
 	public boolean handleMessage(SOAPMessageContext context) {
-		
 		if (isOutbound(context)) {
 			return true;
 		}
@@ -119,13 +101,13 @@ public class WSSRequiredSignatureHandler extends CSSWSHandler {
 		Assert.notEmpty(sigElements, "Unable to find mandatory signature headers in the incoming request (empty) ");
 
 		validSignature = validateSignatures(soapBody, sigElements);
-		logger.debug("Found valid signature: " + validSignature);
+		logger.debug("Found valid signatures: " + validSignature);
 		return validSignature;
 	}
 	
 	private boolean validateSignatures(Element soapBody, List<Element> sigElements ) throws CssWsException {
 		boolean validSignature = false;
-//		boolean assertSigValid = false;
+		boolean assertSigValid = false;
 		boolean wsSigValid = false;
 
 		KeyStore ks = this.getKeystore();
@@ -142,38 +124,65 @@ public class WSSRequiredSignatureHandler extends CSSWSHandler {
 			}
 			
 			for (Element sig : sigElements) {
+				if (wsSigValid && assertSigValid) {
+					break;
+				}
 				
-				if ("Assertion".equalsIgnoreCase(sig.getParentNode().getLocalName())) {
-					logger.debug("Assertion signature found, skipping ");
-				} else {
-					logger.debug("Signature found, trying validation against " + alias);
+				if ("Assertion".equalsIgnoreCase(sig.getParentNode().getLocalName()) && !assertSigValid) {
+					logger.debug("Assertion signature found, trying validation against " + alias);
 					try {
-						wsSigValid = validateSignature(cert, sig, soapBody);
-						if (wsSigValid) {
-							logger.debug("Signature valid for alias " + alias);
-							break;
+						assertSigValid = validateSignature(cert, sig, true, (Element) sig.getParentNode());
+						logger.debug("ASignature valid for alias " + alias + ": " + assertSigValid);
+						if (assertSigValid) {
+							continue;
 						}
-						logger.debug("Signature not valid for alias " + alias);
 					} catch (Exception e) {
-						logger.debug("Signature not valid for alias " + alias + ", exception " + e.getMessage());
+						logger.debug("ASignature not valid for alias " + alias + ", exception " + e.getMessage());
+					}
+				} else if ("Security".equalsIgnoreCase(sig.getParentNode().getLocalName()) && !wsSigValid) {
+					logger.debug("WS Security Signature found, trying validation against " + alias);
+					try {
+						wsSigValid = validateSignature(cert, sig, false, soapBody);
+						logger.debug("WSignature valid for alias " + alias + ": " + wsSigValid);
+						if (wsSigValid) {
+							continue;
+						}
+					} catch (Exception e) {
+						logger.debug("WSignature not valid for alias " + alias + ", exception " + e.getMessage());
 					}
 				}
 			}
 		}
 			
-		validSignature = wsSigValid;
+		validSignature = wsSigValid && assertSigValid;
 		if (!validSignature) {
-			logger.error("Invalid WS Security signature found ");
-			throw new CssWsException("Invalid WS Security signature found ");
+			logger.error("Invalid signature found, WS Sig: " + wsSigValid + ", Assert Sig: " + assertSigValid);
+			throw new CssWsException("Invalid signature found, WS Sig: " + wsSigValid + ", Assert Sig: " + assertSigValid);
 		}
 		
 		return validSignature;
 	}
-
-	private boolean validateSignature(Certificate certificate, Element signature, Element soapBody) throws Exception {
+	
+	private boolean validateSignature(Certificate certificate, Element signature, boolean isAssertion, Element signedNode) throws Exception {
 		PublicKey publicKey = certificate.getPublicKey();
         DOMValidateContext ctx = new DOMValidateContext(publicKey, signature);
-        ctx.setIdAttributeNS(soapBody, "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd", "Id");
+       
+        if (signedNode != null) {
+        	if (isAssertion) {
+            	try {
+            		ctx.setIdAttributeNS(signedNode, null, "ID");
+            	} catch (Exception e) {
+            		logger.error("Unable to set IdAttributeNS ID: " + e.getMessage());
+            	}
+            } else {
+            	try {
+            		ctx.setIdAttributeNS(signedNode, "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd", "Id");
+            	} catch (Exception e) {
+            		logger.error("Unable to set IdAttributeNS ID: " + e.getMessage());
+            	}
+            }
+        }
+        
         XMLSignatureFactory sigF = XMLSignatureFactory.getInstance("DOM");
         XMLSignature sig = sigF.unmarshalXMLSignature(ctx);
         return sig.validate(ctx);
